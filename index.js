7be1a515e1e13a8a80084d8f61cb08ec7df1e597
Tyler Alder <tyler.b.alder@gmail.com>
2020-03-20 08:43:42 -0700

    added getRollCallVote functionality

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 "use strict";
-//const proPubKey = "kR35gMi94d0O1yc4noLkECK7szDsmmCF8csX1cWX";
-// const options = {
-//   headers: new Headers({
-//     "X-Api-Key": proPubKey
-//   })
-// };
+
+const proPubKey = "kR35gMi94d0O1yc4noLkECK7szDsmmCF8csX1cWX";
 
 const apiKey = "AIzaSyAJr43LsKjJhyOQ53CYdNgTqoC8slox5to";
+
+const options = {
+  headers: new Headers({
+    "X-Api-Key": proPubKey
+  })
+};
 
 function displayResults(responseJson) {
   console.log(responseJson);
@@ -45,10 +47,14 @@ function displayResults(responseJson) {
                 }</li>
             </ul>   
         </li>
-        <li class="phone">${responseJson.officials[i].phones}</li>`
+        <li class="phone">${responseJson.officials[i].phones}</li>
+        <button id="get-record" class="${
+          responseJson.officials[i].name
+        }">Get Voting Record</button>`
     );
   }
   $("#results").removeClass("hidden");
+  watchButton();
 }
 
 function getResults(userAddress) {
@@ -70,27 +76,50 @@ function getResults(userAddress) {
 }
 //---------------------------------------------------------------------------------------------------//
 //---------------------------------------------------------------------------------------------------//
+
+//you need an event listener that triggers the following functions
+
 //makes a request for the most recent 20 votes in both chambers
 function getRecentVotes() {
-  const url = "https://api.propublica.org/congress/v1/both/votes/recent.json";
-  fetch(url)
+  const votesUrl =
+    "https://api.propublica.org/congress/v1/both/votes/recent.json";
+  fetch(votesUrl, options)
     .then(response => {
       if (response.ok) {
         return response.json();
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => getIndividualVotesList(responseJson))
+    .then(responseJson => getRollCallVote(responseJson))
     .catch(err => {
       $("#js-error-message").text(`Something went wrong: ${err.message}`);
     });
+  console.log("ran getRecentVotes");
 }
-//iterate through response from getRecentVotes and for each role call number, makes a request for the specific role call vote
-function getIndividualVotesList(responseJson) {
-  const url = "";
+
+//itereate through the response from getRecentVotes and return the individual votes for each roll call number
+function getRollCallVote(responseJson) {
+  for (let i = 0; i < responseJson.results.votes.length; i++) {
+    const rollCallUrl = `https://api.propublica.org/congress/v1/${responseJson.results.votes[i].congress}/${responseJson.results.votes[i].chamber}/sessions/${responseJson.results.votes[i].session}/votes/${responseJson.results.votes[i].roll_call}.json`;
+
+    fetch(rollCallUrl, options)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+          //console.log(response.json);
+        }
+        throw new Error(response.statusText);
+      })
+      .then(responseJson => getIndividualVotes(responseJson))
+      .catch(err => {
+        $("#js-error-message").text(`Something went wrong: ${err.message}`);
+      });
+  }
+  console.log("ran getRollCallVote");
 }
 //returns the specific reps position from the response from getInidividualVotesList
 function getIndividualVotes() {}
+//display the results
 function displayVotingRecord() {}
 
 //---------------------------------------------------------------------------------------------//
@@ -101,6 +130,13 @@ function watchForm() {
     const userAddress = $("#js-address-input").val();
     console.log(userAddress);
     getResults(userAddress);
+  });
+}
+
+function watchButton() {
+  $("#get-record").click(event => {
+    event.preventDefault();
+    getRecentVotes();
   });
 }
 
